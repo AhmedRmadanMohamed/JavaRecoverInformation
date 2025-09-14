b1fb3605980a8fda26c39e2672b47e424c47e5e9
AhmedRmadanMohamed <ahmedramadanmohamedsmaha@gmail.com>
2025-09-14 05:10:49 +0300

    last commit 1492025

--- src/Streams/IntermediateOperations/Main.java
+++ src/Streams/IntermediateOperations/Main.java
@@ -1,18 +1,33 @@
 package Streams.IntermediateOperations;
 
-import static Streams.IntermediateOperations.StreamsIntermediateOperations.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        UseMapFunction().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println();
-        UseMapStrings().forEach(System.out::println);
+//        UseMapFunction().forEach(System.out::println);
+//        System.out.println("--------------------------------");
+//        System.out.println();
+//        UseMapStrings().forEach(System.out::println);
+//        System.out.println("--------------------------------");
+//        System.out.println();
+//        UseMapIntegers().forEach(System.out::println);
+//        int Sum = UseMapToInt("String");
+//        System.out.println(Sum);
+//        double v = UseMapToDouble(1000);
+//        System.out.println(v);
+//        List<List<Integer>> nested = List.of(List.of(1, 2), List.of(3), List.of(4, 5, 6));
+//        nested.forEach(System.out::println);
+//        nested.stream().flatMap(List::stream).forEach(System.out::println);
+//        UseFlatMap().forEach(System.out::println);
+//
+//        UseFlatMapIntegersFilter().forEach(System.out::println);
 
-        System.out.println("--------------------------------");
-        System.out.println();
-        UseMapIntegers().forEach(System.out::println);
 
+        int Number1 = 10;
+        int Number2 = 20;
+        IntStream numbers = Stream.of(Number1, Number1).flatMapToInt(IntStream::of);
+        numbers.forEach(System.out::println);
 
     }
 }
--- src/Streams/IntermediateOperations/StreamsIntermediateOperations.java
+++ src/Streams/IntermediateOperations/StreamsIntermediateOperations.java
@@ -3,6 +3,7 @@ package Streams.IntermediateOperations;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsIntermediateOperations {
 
@@ -32,5 +33,47 @@ public class StreamsIntermediateOperations {
 
     }
 
+    public static int UseMapToInt(String input) {
 
+        return Stream.of(input).mapToInt(String::length).sum();
+    }
+
+
+    public static Double UseMapToDouble(int input) {
+        return Stream.of(input).mapToDouble(Integer::doubleValue).sum();
+    }
+
+
+    public static List<Integer> UseFlatMap() {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list1.add(i * 10);
+            list2.add(i * i);
+        }
+        System.out.println("Out FlatMap List1: " + list1);
+        System.out.println();
+        System.out.println("-----------------------------------");
+        System.out.println();
+        System.out.println("Out FlatMap list2: " + list2);
+
+        return Stream.of(list1, list2).flatMap(List::stream).collect(Collectors.toList());
+    }
+
+
+    public static List<Integer> UseFlatMapIntegersFilter() {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list1.add(i * 10);
+            list2.add(i * i);
+        }
+        System.out.println("Out FlatMap List1: " + list1);
+        System.out.println();
+        System.out.println("-----------------------------------");
+        System.out.println();
+        System.out.println("Out FlatMap list2: " + list2);
+        System.out.println();
+        return Stream.of(list1, list2).flatMap(List::stream).filter(A -> A > 10).distinct().collect(Collectors.toList());
+    }
 }
